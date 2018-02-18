2fafccc2c63ca3f0483eeb07235889f5d4e6ed11
shaynak <shayna.kothari@gmail.com>
2018-02-18 08:18:01 -0800

    finished site! time for optimization l o l

--- treehacks.js
+++ treehacks.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 	"use strict";
 	var tickerNames2 = ["MMM","ACN","AFL","V","VHS","VVV","VNTR","VNTV","WY","WYY","WHR",
-	"WIT","XRX","AJG","APTS","APTV","ATR","AVT","ANTM","ANTX","CAJ","CL","CMI","DNB","WD","DVMT",
-	"EMN","ECL","ECR","EW","EWJ","EWW","LLY","GE","DNA","GER","GNRC","GNRT","G","GDI","GLOP",
-	"GTES","FLR","HASI","HCA","HLT","HGV","JCI","DAR","IP","IPSC","IPOA U","VXX","JILL","LDOS",
+	"WIT","XRX","AJG","APTS","ATR","AVT","ANTM","ANTX","CAJ","CL","CMI","DNB","WD","DVMT",
+	"EMN","ECL","ECR","EW","EWJ","EWW","LLY","GE","GER","GNRC","GNRT","G","GDI","GLOP",
+	"GTES","FLR","HASI","HCA","HLT","HGV","JCI","DAR","IP","IPSC","VXX","JILL","LDOS",
 	"KMT","MSO","GNRT","M","MMD","TUC","NGG","PXD","NOK","OSK","DPS","PX","PXD","PFGC","RLGY",
 	"PBIPB","ROK","RSG","CRM","SNDR","TEL","TDOC","TDC","AES","TKR"];
 	$("#stockSearch").keyup(function() {
